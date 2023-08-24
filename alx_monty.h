24f6b49e3ac9277eda88fe09f3373187d5957167
alban-okoby <albanokoby225@gmail.com>
2023-08-24 15:59:09 -0700

    Issue of string librairie

--- alx_monty.h
+++ alx_monty.h
@@ -19,12 +19,26 @@
  * Description: Doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
-typedef struct alx_stack_s
+typedef struct stack_s
 {
 	int n;
-	struct alx_stack_s *prev;
-	struct alx_stack_s *next;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
